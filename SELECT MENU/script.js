aec253439662e307984fa1c412aaab54f45e3e7b
ferliSuganda <160320483+DeoVindice29@users.noreply.github.com>
2024-03-16 22:52:24 +0700

    Add files via upload

---
+++ SELECT MENU/script.js
@@ -0,0 +1,18 @@
+var options = document.getElementsByClassName("options");
+var selectField = document.getElementById("selectField");
+var selectText = document.getElementById("selectText");
+var arrowIcon = document.getElementById("arrowIcon");
+var list = document.getElementById("list");
+
+selectField.onclick = function () {
+  list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
+};
+
+for (option of options) {
+  option.onclick = function () {
+    selectText.innerHTML = this.textContent;
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+  };
+}
