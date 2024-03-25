dcb360077916005541a3248f93fea915b7f1f71e
Ferli Suganda <160320483+DeoVindice29@users.noreply.github.com>
2024-03-25 19:52:29 +0700

    Add files via upload

---
+++ DETECT ADBLOCK/script.js
@@ -0,0 +1,30 @@
+const detect = document.querySelector("#detect"),
+  wrapper = document.querySelector(".wrapper"),
+  button = wrapper.querySelector("button");
+
+let adClasses = [
+  "ad",
+  "ads",
+  "adsbox",
+  "ad-placement",
+  "doubleclick",
+  "ad-placeholder",
+  "ad-badge",
+  "AdBlock",
+];
+for (let item of adClasses) {
+  detect.classList.add(item);
+}
+
+let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
+
+button.addEventListener("click", () => {
+  wrapper.classList.remove("show");
+});
+
+if (!wrapper.classList.contains("show")) {
+  getProperty == "none"
+    ? wrapper.classList.add("show")
+    : wrapper.classList.remove("show");
+}
+console.log(getProperty);
