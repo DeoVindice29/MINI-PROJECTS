2eb88d2051d4631bcc5eefdf416cc9119fde5086
Ferli Suganda <160320483+DeoVindice29@users.noreply.github.com>
2024-10-27 15:09:00 +0700

    Update script.js

--- AGE-CALCULATOR-V2/script.js
+++ AGE-CALCULATOR-V2/script.js
@@ -15,28 +15,35 @@ modeSwitch.addEventListener("click", () => {
 });
 
 function age() {
-  var d1 = document.getElementById("date").value;
-  var m1 = document.getElementById("month").value;
-  var y1 = document.getElementById("year").value;
 
-  var date = new Date();
-  var d2 = date.getDate();
-  var m2 = 1 + date.getMonth();
-  var y2 = date.getFullYear();
-  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  if (d1 > d2) {
-    d2 = d2 + month[m2 - 1];
-    m2 = m2 - 1;
+  if (!d1 || !m1 || !y1) {
+    alert("Please enter a valid date before clicking Submit.");
+    return;
   }
-  if (m1 > m2) {
-    m2 = m2 + 12;
-    y2 = y2 - 1;
-  }
-  var d = d2 - d1;
-  var m = m2 - m1;
-  var y = y2 - y1;
+  else {
+    var d1 = document.getElementById("date").value;
+    var m1 = document.getElementById("month").value;
+    var y1 = document.getElementById("year").value;
 
-  document.getElementById("age").innerHTML =
-    "Your Age is " + y + " Years " + m + " Months & " + d + " Days";
+    var date = new Date();
+    var d2 = date.getDate();
+    var m2 = 1 + date.getMonth();
+    var y2 = date.getFullYear();
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (d1 > d2) {
+      d2 = d2 + month[m2 - 1];
+      m2 = m2 - 1;
+    }
+    if (m1 > m2) {
+      m2 = m2 + 12;
+      y2 = y2 - 1;
+    }
+    var d = d2 - d1;
+    var m = m2 - m1;
+    var y = y2 - y1;
+
+    document.getElementById("age").innerHTML =
+      "Your Age is " + y + " Years " + m + " Months & " + d + " Days";
+  }
 }
