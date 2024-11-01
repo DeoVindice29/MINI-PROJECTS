a9e3b7addcddd5bb3c1faf46bb6ea7c894575abd
Ferli Suganda <160320483+DeoVindice29@users.noreply.github.com>
2024-11-01 20:12:19 +0700

    Delete CHANGE-PROFILE directory

--- CHANGE-PROFILE/script.js
+++
@@ -1,8 +0,0 @@
-let profilePic = document.getElementById("profile-pic");
-let inputFile = document.getElementById("input-file");
-
-inputFile.onchange = function () {
-  profilePic.src = URL.createObjectURL(inputFile.files[0]);
-  profilePic.style.aspectRatio = "1/1";
-  profilePic.style.objectFit = "cover";
-};
