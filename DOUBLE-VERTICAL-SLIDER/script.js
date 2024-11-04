4d28e78357ae23177b426d22a3279cae4655e816
Ferli Suganda <160320483+DeoVindice29@users.noreply.github.com>
2024-11-04 12:11:43 +0700

    Update script.js

--- DOUBLE-VERTICAL-SLIDER/script.js
+++ DOUBLE-VERTICAL-SLIDER/script.js
@@ -7,7 +7,6 @@ const slidesLength = slideRight.querySelectorAll("div").length;
 
 let activeSlideIndex = 0;
 
-// Pindahkan perhitungan style.top ke luar fungsi untuk efisiensi
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
 upButton.addEventListener("click", () => changeSlide("up"));
@@ -16,17 +15,14 @@ downButton.addEventListener("click", () => changeSlide("down"));
 const changeSlide = (direction) => {
   const sliderHeight = sliderContainer.clientHeight;
 
-  // Perbarui activeSlideIndex berdasarkan arah dan batasi nilai
   activeSlideIndex = direction === "up" ?
     (activeSlideIndex + 1) % slidesLength :
     (activeSlideIndex - 1 + slidesLength) % slidesLength;
 
-  // Terapkan transformasi slide dengan nilai activeSlideIndex yang diperbarui
   slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
   slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
 };
 
-// Tambahkan event listener keyup di luar fungsi changeSlide untuk efisiensi
 window.addEventListener("keyup", (event) => {
   if (["ArrowUp", "ArrowDown"].includes(event.key)) {
     changeSlide(event.key === "ArrowUp" ? "up" : "down");
