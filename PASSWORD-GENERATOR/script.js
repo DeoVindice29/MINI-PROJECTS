c41b30a5b62837aabdc0699192bfeddb8eae2a04
Ferli Suganda <160320483+DeoVindice29@users.noreply.github.com>
2024-12-02 13:52:31 +0700

    Add files via upload

---
+++ PASSWORD-GENERATOR/script.js
@@ -0,0 +1,19 @@
+const passwordBox = document.getElementById("password");
+
+function createPassword() {
+  const length = document.getElementById('length').value;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~|{}[]<>/-=?";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  document.getElementById('password').value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
